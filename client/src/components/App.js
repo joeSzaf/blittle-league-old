41a7f3c38f67313b7b236957cc8cd0b52b36e051
Joe Szaf <joeszaf+github@gmail.com>
2021-04-05 00:38:19 -0400

    first draft of homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,36 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
+import React, { useState, useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { hot } from "react-hot-loader/root"
 
-import getCurrentUser from "../services/getCurrentUser";
-import "../assets/scss/main.scss";
-import RegistrationForm from "./registration/RegistrationForm";
-import SignInForm from "./authentication/SignInForm";
-import TopBar from "./layout/TopBar";
+import getCurrentUser from "../services/getCurrentUser"
+import "../assets/scss/main.scss"
+import RegistrationForm from "./registration/RegistrationForm"
+import SignInForm from "./authentication/SignInForm"
+import TopBar from "./layout/TopBar"
+import BottomBar from "./layout/BottomBar"
+import HomePage from "./pages/HomePage"
+
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#ffef62',
+      main: '#ffeb3b',
+      dark: '#b2a429',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ffef62',
+      main: '#ffeb3b',
+      dark: '#b2a429',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    fontFamily: 'Lora, serif',
+    fontColor: 'white'
+  },
+})
 
 const App = (props) => {
-  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined)
   useEffect(() => {
     getCurrentUser()
       .then((user) => {
-        setCurrentUser(user);
+        setCurrentUser(user)
       })
       .catch(() => {
-        setCurrentUser(null);
-      });
-  }, []);
+        setCurrentUser(null)
+      })
+  }, [])
+  
   return (
     <Router>
-      <TopBar user={currentUser} />
-      <Switch>
-        <Route exact path="/">
-          <h2>Hello from react</h2>
-        </Route>
-        <Route exact path="/users/new" component={RegistrationForm} />
-        <Route exact path="/user-sessions/new" component={SignInForm} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <TopBar user={currentUser} />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/users/new" component={RegistrationForm} />
+          <Route exact path="/user-sessions/new" component={SignInForm} />
+        </Switch>
+        <BottomBar />
+      </ThemeProvider>
     </Router>
-  );
-};
+  )
+}
 
-export default hot(App);
+export default hot(App)
--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,8 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SignOutButton from "../authentication/SignOutButton";
+import React from "react"
+import { Link } from "react-router-dom"
+import SignOutButton from "../authentication/SignOutButton"
+
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+import SportsBaseball from '@material-ui/icons/SportsBaseball'
+
+const useStyles = makeStyles((theme) => ({
+  topBar: {
+    backgroundColor: '#222222',
+    fontColor: '#FFFFFF',
+    padding: theme.spacing(1),
+  },
+  title: {
+    padding: theme.spacing(1),
+  },
+}))
 
 const TopBar = ({ user }) => {
+  const classes = useStyles()
+
   const unauthenticatedListItems = [
     <li key="sign-in">
       <Link to="/user-sessions/new">Sign In</Link>
@@ -12,29 +33,44 @@ const TopBar = ({ user }) => {
         Sign Up
       </Link>
     </li>,
-  ];
+  ]
 
   const authenticatedListItems = [
     <li key="sign-out">
       <SignOutButton />
     </li>,
-  ];
+  ]
 
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">App</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
-      </div>
+    <div>
+      <AppBar position="fixed" className={classes.topBar}>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <SportsBaseball fontSize="large" color="action" />
+            <Typography variant="h4" className={classes.title} color="textPrimary" noWrap>
+              Blittle League
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar />
     </div>
-  );
-};
+    
 
-export default TopBar;
+    // <div className="top-bar">
+    //   <div className="top-bar-left">
+    //     <ul className="menu">
+    //       <li className="menu-text">App</li>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   <div className="top-bar-right">
+    //     <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
+    //   </div>
+    // </div>
+  )
+}
+
+export default TopBar
